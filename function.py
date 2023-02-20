c2360643a3480c44d9c95786047c2965980ba93c
Jonespeterdev <joneshosting.usts@gmail.com>
2023-02-20 17:21:01 +0530

    new update

---
+++ function.py
@@ -0,0 +1,13 @@
+def function1():
+    print("function 1 called")
+def function2():
+    print("function 2 called")
+
+if __name__ == '__main__':
+    inputvar=int(input("1:FUNCTION 1\n 2:FUNCTION 2\nENTER YOUR CHOICE : "))
+    if inputvar==1:
+        function1()
+    elif inputvar==2:
+        function2()
+    else:
+        print("Wrong choice..!")
