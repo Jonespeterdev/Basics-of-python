d2cefdaa4d7d53e5b106ce3f9b5434557e42bc44
Jonespeterdev <joneshosting.usts@gmail.com>
2023-03-02 12:41:32 +0530

    Basic Python

--- subprocess.py
+++ subprocess.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 def listdir(dir):
-  cmd = 'dir' + dir
+  cmd = 'dir ' + dir
   print("Command to run:", cmd)
   (status, output) = subprocess.getstatusoutput(cmd)
   if status:    ## Error case, print the command's output to stderr and exit
@@ -10,4 +10,4 @@ def listdir(dir):
     sys.exit(status)
   print(output)
 
-listdir("path/of/your/folder")
+listdir("E:/jones")
